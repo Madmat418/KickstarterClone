19326679bd805ad6c5149f684c3ac197eb1f05be
Matthew <MadMat41888@aol.com>
2014-01-16 10:57:04 -0800

    Began converting to backbone, created backbone projects router

---
+++ app/assets/javascripts/kickstarter.js
@@ -0,0 +1,26 @@
+window.Kickstarter = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialze: function () {
+    var $rootEl = $('#content');
+    var $sidebar = $('#sidebar');
+    var projects = new Kickstarter.Collections.Projects();
+
+    var sidebarView = new Kickstarter.Views.sidebarView();
+    $sidebar.html(sidebarView.render().$el);
+
+    projects.fetch({
+      success: function () {
+        new Kickstarter.Routers.ProjectRouter(projects, $rootEl);
+        Backbone.history.start();
+      },
+      error: function () {
+        console.log('Experienced an error in fetching data');
+      }
+    });
+  }
+};
+
+$(Kickstarter.initialize);
---
+++ app/assets/javascripts/routers/project_router.js
@@ -0,0 +1,49 @@
+Kickstarter.Routers.ProjectRouter = Backbone.Router.extend ( {
+  initialize: function (projects, $rootEl) {
+    this.projects = projects;
+    this.$rootEl = $rootEl;
+  },
+
+  routes: {
+    '': 'index',
+    'projects/:category': 'browse',
+    'projects/:id': 'show',
+    'projects/new': 'new'
+  },
+
+  index: function () {
+    var projectIndex = new Kickstarter.Views.ProjectIndex ( {
+      category: 'all';
+    });
+
+    this._swapView(projectIndex);
+  },
+
+  browse: function (category) {
+    var categoryIndex = new Kickstarter.Views.ProjectIndex ( {
+      category: category;
+    });
+
+    this._swapView(categoryIndex);
+  },
+
+  show: function (id) {
+    var projectShow = new Kickstarter.Views.ProjectShow ( {
+      model: this.projects.get(id);
+    });
+
+    this._swapView(projectShow);
+  },
+
+  new: function () {
+    var newProject = new Kickstarter.Views.NewProject();
+
+    this._swapView(newProject);
+  },
+
+  _swapView: function (view) {
+    this._currentView && this._currentView.remove();
+    this._currentView = view;
+    this.$rootEl.html(view.render().$el);
+  }
+});
